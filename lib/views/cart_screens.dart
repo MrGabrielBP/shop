af09ec46c2964e7b3c9c04f2c71251ac1a6a5092
MrGabrielBP <g.porto@unesp.br>
2021-01-17 11:44:27 -0300

    criada função para adicionar pedidos

--- lib/views/cart_screens.dart
+++ lib/views/cart_screens.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/provider/cart.dart';
+import 'package:shop/provider/orders.dart';
 import 'package:shop/widgets/cart_item_widget.dart';
 
 class CartScreen extends StatelessWidget {
@@ -40,7 +41,11 @@ class CartScreen extends StatelessWidget {
                   ),
                   Spacer(), //Ele vai ocupar o resto do espaço. Colocando o Total e o valor juntos.
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false)
+                          .addOrder(cart);
+                      cart.clear();
+                    },
                     textColor: Theme.of(context).primaryColor,
                     child: Text("COMPRAR"),
                   )
