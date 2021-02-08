b362333ed5ab21089c0e31624b7609ef0ab485ee
MrGabrielBP <g.porto@unesp.br>
2021-02-07 22:49:02 -0300

    implementado Pull-To-Refresh

--- lib/views/product_screen.dart
+++ lib/views/product_screen.dart
@@ -6,6 +6,10 @@ import 'package:shop/widgets/app_drawer.dart';
 import 'package:shop/widgets/product_item.dart';
 
 class ProductScreen extends StatelessWidget {
+  Future<void> _refreshProducts(BuildContext context) {
+    return Provider.of<Products>(context, listen: false).loadProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final products = Provider.of<Products>(context);
@@ -23,18 +27,21 @@ class ProductScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(8),
-        child: ListView.builder(
-          itemCount: products.itemsCount,
-          itemBuilder: (ctx, i) {
-            return Column(
-              children: [
-                ProductItem(productItems[i]),
-                Divider(),
-              ],
-            );
-          },
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListView.builder(
+            itemCount: products.itemsCount,
+            itemBuilder: (ctx, i) {
+              return Column(
+                children: [
+                  ProductItem(productItems[i]),
+                  Divider(),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
