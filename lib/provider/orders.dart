bd68fed9710ba0e5b0e64aaf351a69598b8f2b74
MrGabrielBP <gabrielbastos.10@hotmail.com>
2021-02-18 14:53:43 -0300

    carregando os pedidos

--- lib/provider/orders.dart
+++ lib/provider/orders.dart
@@ -29,6 +29,36 @@ class Orders with ChangeNotifier {
     return _items.length;
   }
 
+  Future<void> loadOrders() async {
+    List<Order> loadedItems = [];
+    final response = await http.get("$_baseUrl.json");
+    Map<String, dynamic> data = json.decode(response.body);
+    //Dar print para visualizar a response!!!
+    if (data != null) {
+      data.forEach((orderID, orderData) {
+        loadedItems.add(
+          Order(
+            id: orderID,
+            total: orderData['total'],
+            date: DateTime.parse(orderData['date']),
+            products: (orderData['products'] as List<dynamic>).map((item){
+              return CartItem(
+                id: item['id'], 
+                productId: item['productId'], 
+                title: item['title'], 
+                quantity: item['quantity'], 
+                price: item['price'],
+                );
+            }).toList()
+          ),
+        );
+      });
+      notifyListeners();
+    }
+    _items = loadedItems.reversed.toList();
+    return Future.value();
+  }
+
   Future<void> addOrder(Cart cart) async {
     final date = DateTime.now();
     final response = await http.post(
