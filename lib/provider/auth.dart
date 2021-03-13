5709c1ae91abcb93acdc9a80764466ba2dd00cc0
MrGabrielBP <gabrielbastos.10@hotmail.com>
2021-03-13 08:13:06 -0300

    logout automático

--- lib/provider/auth.dart
+++ lib/provider/auth.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -12,6 +13,7 @@ class Auth with ChangeNotifier {
   String _userId;
   String _token;
   DateTime _expireDate;
+  Timer _logoutTimer;
 
   bool get isAuth {
     return token != null;
@@ -54,6 +56,7 @@ class Auth with ChangeNotifier {
           seconds: int.parse(responseBody["expiresIn"]),
         ),
       );
+      _autoLogout();
       notifyListeners();
     }
 
@@ -72,6 +75,17 @@ class Auth with ChangeNotifier {
     _token = null;
     _userId = null;
     _expireDate = null;
+    if (_logoutTimer != null) {
+      _logoutTimer.cancel();
+      _logoutTimer = null;
+    }
     notifyListeners();
+  }
+
+  void _autoLogout() {
+    if (_logoutTimer != null) _logoutTimer.cancel();
+
+    final timeToLogout = _expireDate.difference(DateTime.now()).inSeconds;
+    _logoutTimer = Timer(Duration(seconds: timeToLogout), logout);
   }
 }
